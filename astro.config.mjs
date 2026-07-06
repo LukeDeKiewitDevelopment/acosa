02c05dbff479ed8c77b114b5d07485c9e8f23188
Luke De Kiewit <79079523+lukedekiewit@users.noreply.github.com>
2026-07-06 15:45:34 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-import node from "@astrojs/node";
 import keystatic from "@keystatic/astro";
 import vercel from '@astrojs/vercel';
 
