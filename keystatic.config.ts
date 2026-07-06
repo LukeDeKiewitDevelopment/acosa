c54670584fbcf6845d033154a0e1e02fefa1d8ff
Luke De Kiewit <79079523+lukedekiewit@users.noreply.github.com>
2026-07-06 15:13:12 +0200

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,7 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'cloud',
   },
   collections: {
     posts: collection({
