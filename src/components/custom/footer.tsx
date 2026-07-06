3b2e7cfb4b4ff2a8557a92daa47c38f1719c9050
Luke De Kiewit <79079523+lukedekiewit@users.noreply.github.com>
2026-07-06 15:09:43 +0200

    update

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = ({
 }: FooterProps) => {
 return (
     <footer className="w-full px-8 py-4 bg-primary text-primary-foreground">
-        <p>&copy; ACOSA. All rights reserved. Work in Progress</p>
+        <p className="text-xs">&copy; ACOSA. Work in Progress.</p>
     </footer>
 )
 }
