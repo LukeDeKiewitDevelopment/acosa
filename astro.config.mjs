00f3d00f51f0efe94d1cede776a83405c93aa77a
Luke De Kiewit <79079523+lukedekiewit@users.noreply.github.com>
2026-07-06 13:16:28 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,17 @@ import markdoc from '@astrojs/markdoc';
 import node from '@astrojs/node';
 import keystatic from '@keystatic/astro';
 
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
