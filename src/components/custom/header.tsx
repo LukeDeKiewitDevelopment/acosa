637a1839b7e7f07b61cd0544e8beb6a0c48b5bbd
Luke De Kiewit <79079523+lukedekiewit@users.noreply.github.com>
2026-07-08 14:03:06 +0200

    update

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -1,8 +1,28 @@
+import { Button } from "../ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+
 export type HeaderProps = {
   logo?: string;
   logoAlt?: string;
   logoDark?: string;
   logoDarkAlt?: string;
+  navItems?: NavItem[];
+};
+export type NavItem = {
+  label?: string;
+  href?: string;
+  subItems?: NavSubItem[];
+};
+export type NavSubItem = {
+  categoryName?: string;
+  categoryHref?: string;
+  items?: NavItem[];
 };
 
 export const Header = ({
@@ -10,15 +30,71 @@ export const Header = ({
   logoAlt,
   logoDark,
   logoDarkAlt,
+  navItems,
   ...props
 }: HeaderProps) => {
   return (
     <header
       data-slot="header"
-      className="bg-card text-card-foreground sticky top-0 left-0 z-50 border-b-2 px-4 py-1 md:px-6 lg:px-8"
+      className="bg-card text-card-foreground sticky top-0 left-0 z-50 border-b-2 px-4 py-2 md:px-6 lg:px-8"
       {...props}
     >
-      <div className="flex items-center justify-between gap-4"></div>
+      <div className="flex items-center justify-between gap-4">
+        {navItems && (
+          <ul className="flex items-center gap-2">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                {item.subItems && item.subItems.length > 0 && (
+                  <NavigationMenu>
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger>
+                          {item.label}
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                          <ul className="w-96">
+                            {item.subItems.map((subItem, subIndex) => (
+                              <li key={subIndex}>
+                                <a
+                                  href={subItem.categoryHref}
+                                  className="no-underline"
+                                >
+                                  {subItem.categoryName}
+                                </a>
+                                {subItem.items && subItem.items.length > 0 && (
+                                  <ul>
+                                    {subItem.items.map((node, nodeIndex) => (
+                                      <li key={nodeIndex}>
+                                        <a
+                                          href={node.href}
+                                          className="no-underline"
+                                        >
+                                          {node.label}
+                                        </a>
+                                      </li>
+                                    ))}
+                                  </ul>
+                                )}
+                              </li>
+                            ))}
+                          </ul>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
+                )}
+                {item.href && item.label && !item.subItems && (
+                  <Button variant="ghost" asChild>
+                    <a className="no-underline" href={item.href}>
+                      {item.label}
+                    </a>
+                  </Button>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </header>
   );
 };
